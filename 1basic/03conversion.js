21b0ef84d23520394973b4ac420361425a4fcf67
kartik <142092867+kartik7712@users.noreply.github.com>
2024-01-25 17:37:41 +0530

    Add files via upload

--- 1basic/03conversion.js
+++ 1basic/03conversion.js
@@ -16,18 +16,50 @@ console.log(newnumber)
 // "string" = NaN
 
 // convert data to string
-let isstring = "kartik"
-let newstring = String(isstring)
-console.log( typeof newstring)
-console.log(newstring)
+// let isstring = "kartik"
+// let newstring = String(isstring)
+// console.log( typeof newstring)
+// console.log(newstring)
 // 63 = string //convert data into string
 
 // conver data to Boolean
-let isvalue = ""
-let newboolean = Boolean(isvalue)
-console.log(newboolean)
+// let isvalue = ""
+// let newboolean = Boolean(isvalue)
+// console.log(newboolean)
 
 // 1=true / 0=false
 // "" = false
 // "689hks" = true
+
+// **************************************// conversion operation
+
+// console.log(3+3);
+// console.log(3 +  '3');
+// console.log('3' +  '3');
+
+// console.log('3' +  3 +3); majority of add is high
+// console.log(3 +  3 + '3'); 
+
+// console.log(3+3)
+// console.log(3 * 3)
+// console.log(3 - 3)
+// console.log(3 ** 2)
+// console.log(3 / 3)
+// console.log(3 % 3)
+// console.log(3 + 3 - 2 * 5 / 2)
+
+console.log(+ true)
+// console.log(+ "")
+
+//  let num1 , num2 , num3;
+//  num1 = num2 = num3 = 2 + 2; //never use this type os complexity
+//  console.log(num1);
+
+let game = 100;
+// prefix
+++game;
+
+// postfix
+game++;
+console.log(game)
 
